cb71c3eaedd84eadd61a7cfe2dd3a0e96ef72486
Григорий Ким <kim-g@yandex.ru>
2017-07-04 14:28:51 +0500

    Сделал получение настроек камеры

---
+++ Camera_Config/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Camera_Config
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.videoResolutionsCombo = new System.Windows.Forms.ComboBox();
+            this.snapshotResolutionsCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(87, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(233, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // videoResolutionsCombo
+            // 
+            this.videoResolutionsCombo.FormattingEnabled = true;
+            this.videoResolutionsCombo.Location = new System.Drawing.Point(87, 58);
+            this.videoResolutionsCombo.Name = "videoResolutionsCombo";
+            this.videoResolutionsCombo.Size = new System.Drawing.Size(233, 21);
+            this.videoResolutionsCombo.TabIndex = 1;
+            this.videoResolutionsCombo.SelectedIndexChanged += new System.EventHandler(this.videoResolutionsCombo_SelectedIndexChanged);
+            // 
+            // snapshotResolutionsCombo
+            // 
+            this.snapshotResolutionsCombo.FormattingEnabled = true;
+            this.snapshotResolutionsCombo.Location = new System.Drawing.Point(87, 85);
+            this.snapshotResolutionsCombo.Name = "snapshotResolutionsCombo";
+            this.snapshotResolutionsCombo.Size = new System.Drawing.Size(233, 21);
+            this.snapshotResolutionsCombo.TabIndex = 2;
+            this.snapshotResolutionsCombo.SelectedIndexChanged += new System.EventHandler(this.snapshotResolutionsCombo_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = " ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(326, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Режим фото";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Режим видео";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Камера";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 146);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.snapshotResolutionsCombo);
+            this.Controls.Add(this.videoResolutionsCombo);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox videoResolutionsCombo;
+        private System.Windows.Forms.ComboBox snapshotResolutionsCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Camera_Config/Form1.cs
@@ -0,0 +1,84 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Camera_Config
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoDevice;
+        private VideoCapabilities[] videoCapabilities;
+        private VideoCapabilities[] snapshotCapabilities;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (this.videoDevices.Count != 0)
+            {
+                foreach (FilterInfo videoDevice1 in (CollectionBase)this.videoDevices)
+                    this.comboBox1.Items.Add((object)videoDevice1.Name);
+            }
+            else
+                this.comboBox1.Items.Add((object)"No DirectShow devices found");
+        }
+
+        private void EnumeratedSupportedFrameSizes(VideoCaptureDevice videoDevice)
+        {
+            
+
+            this.Cursor = Cursors.WaitCursor;
+            this.videoResolutionsCombo.Items.Clear();
+            this.snapshotResolutionsCombo.Items.Clear();
+            try
+            {
+                this.videoCapabilities = videoDevice.VideoCapabilities;
+                this.snapshotCapabilities = videoDevice.SnapshotCapabilities;
+                foreach (VideoCapabilities videoCapability in this.videoCapabilities)
+                    this.videoResolutionsCombo.Items.Add((object)string.Format("{0} x {1}", (object)videoCapability.FrameSize.Width, (object)videoCapability.FrameSize.Height));
+                foreach (VideoCapabilities snapshotCapability in this.snapshotCapabilities)
+                    this.snapshotResolutionsCombo.Items.Add((object)string.Format("{0} x {1}", (object)snapshotCapability.FrameSize.Width, (object)snapshotCapability.FrameSize.Height));
+                if (this.videoCapabilities.Length == 0)
+                    this.videoResolutionsCombo.Items.Add((object)"Not supported");
+                if (this.snapshotCapabilities.Length == 0)
+                    this.snapshotResolutionsCombo.Items.Add((object)"Not supported");
+                this.videoResolutionsCombo.SelectedIndex = -1;
+                this.snapshotResolutionsCombo.SelectedIndex = -1;
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.videoDevices.Count == 0)
+                return;
+            this.videoDevice = new VideoCaptureDevice(this.videoDevices[this.comboBox1.SelectedIndex].MonikerString);
+            this.EnumeratedSupportedFrameSizes(this.videoDevice);
+
+            label1.Text = comboBox1.SelectedIndex.ToString();
+        }
+
+        private void videoResolutionsCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label2.Text = videoResolutionsCombo.SelectedIndex.ToString();
+        }
+
+        private void snapshotResolutionsCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label3.Text = snapshotResolutionsCombo.SelectedIndex.ToString();
+        }
+    }
+}
